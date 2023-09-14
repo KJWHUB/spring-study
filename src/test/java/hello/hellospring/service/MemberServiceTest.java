2ef12171e3d547c1638abbb04102dee526cd0673
kjw1025_mac_studio <126055193+KJWHUB@users.noreply.github.com>
2023-09-14 15:03:27 +0900

    fix: 회원 서비스 테스트 수정

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MemberServiceTest {
 
-    MemberService memberService
+    MemberService memberService;
     MemoryMemberRepository memberRepository;
 
     @BeforeEach
